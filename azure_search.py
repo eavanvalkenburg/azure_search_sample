a9d091c61876ae627d67bf43b540e49f78e9ccee
Eduard van Valkenburg <edvan@microsoft.com>
2019-05-13 13:03:54 +0200

    fixed docstrings

--- azure_search.py
+++ azure_search.py
@@ -1,9 +1,13 @@
+"""Classes for communicating with Azure Search."""
 import requests
 import time
-# import msrest
+
 
 class AzureSearchConfig:
-    def __init__(self,
+    """Contains the config of a AzureSearch class."""
+
+    def __init__(
+            self,
             search_service_name,
             search_api_key,
             search_api_version,
@@ -12,7 +16,9 @@ class AzureSearchConfig:
             datasource,
             index,
             skillset,
-            indexer):
+            indexer,
+            **kwargs):
+        """Create a AzureSearch config."""
         self.search_service_name = search_service_name
         self.search_api_key = search_api_key
         self.search_api_version = search_api_version
@@ -22,85 +28,120 @@ class AzureSearchConfig:
         self.index = index
         self.skillset = skillset
         self.indexer = indexer
+        if 'domain' in kwargs:
+            self._domain = kwargs['domain']
+        else:
+            self._domain = 'search.windows.net/'
 
-    def get_base_url(self, domain='search.windows.net/'):
-        return f"https://{self.search_service_name}.{domain}"
+    @property
+    def base_url(self):
+        """Return the base url of the api."""
+        return f"https://{self.search_service_name}.{self._domain}"
+
 
 class AzureSearch:
+    """Creates an instance of AzureSearch."""
+
     def __init__(self, config: AzureSearchConfig, debug=False):
+        """Create an instance of AzureSearch."""
         self.config = config
         self.debug = debug
-        self.base_url = self.config.get_base_url()
         self.base_payload = {"api-version": self.config.search_api_version}
-        self.base_headers = {"api-key": self.config.search_api_key, "Content-Type": "application/json"}
+        self.base_headers = {
+            "api-key": self.config.search_api_key,
+            "Content-Type": "application/json"}
 
-    #create datasource
     def create_datasource(self, body, **kwargs):
-        body['credentials']['connectionString'] = self.config.storage_connection_string
+        """Create a datasource in the search service."""
+        body['credentials']['connectionString'] = \
+            self.config.storage_connection_string
         body['name'] = self.config.datasource
 
         if self.debug:
             print(body)
-        url = self.base_url + f"datasources/{self.config.datasource}"
-        req = requests.put(url, headers=self.base_headers, json=body, params=self.base_payload, **kwargs)
+        url = self.config.base_url + f"datasources/{self.config.datasource}"
+        req = requests.put(
+            url, headers=self.base_headers,
+            json=body, params=self.base_payload,
+            **kwargs)
         if self.debug:
             print(req.text)
         req.raise_for_status()
 
-    #create index
     def create_index(self, body, **kwargs):
+        """Create a index in the search service."""
         body['name'] = self.config.index
 
         # try delete
-        url = self.base_url + f"indexes/{self.config.index}"
-        delete = requests.delete(url, headers=self.base_headers, params=self.base_payload, **kwargs)
-
-        req = requests.put(url, headers=self.base_headers, json=body, params=self.base_payload, **kwargs)
+        url = self.config.base_url + f"indexes/{self.config.index}"
+        delete = requests.delete(
+            url, headers=self.base_headers,
+            params=self.base_payload, **kwargs)
+        if self.debug:
+            print(delete.status_code)
+        req = requests.put(
+            url, headers=self.base_headers,
+            json=body, params=self.base_payload, **kwargs)
         req.raise_for_status()
         if self.debug:
             print(req.text)
 
-    #create skillset
     def create_skillset(self, body, **kwargs):
+        """Create a skillset in the search service."""
         body['name'] = self.config.skillset
         if 'cognitiveServices' in body:
-            body['cognitiveServices']['key'] = self.config.cognitive_services_key        
-        if 'knowledgeStore' in body: 
-            body['knowledgeStore']['storageConnectionString'] = self.config.storage_connection_string
+            body['cognitiveServices']['key'] = \
+                self.config.cognitive_services_key
+        if 'knowledgeStore' in body:
+            body['knowledgeStore']['storageConnectionString'] = \
+                self.config.storage_connection_string
 
         if self.debug:
             print(body)
-        url = self.base_url + f"skillsets/{self.config.skillset}"
-        req = requests.put(url, headers=self.base_headers, json=body, params=self.base_payload,  **kwargs)
+        url = self.config.base_url + f"skillsets/{self.config.skillset}"
+        req = requests.put(
+            url, headers=self.base_headers,
+            json=body, params=self.base_payload,  **kwargs)
         if self.debug:
             print(req.text)
         req.raise_for_status()
 
-    #create indexer
     def create_indexer(self, body, **kwargs):
+        """Create a indexer in the search service."""
         body['name'] = self.config.indexer
         body['dataSourceName'] = self.config.datasource
         body['skillsetName'] = self.config.skillset
         body['targetIndexName'] = self.config.index
 
-        url = self.base_url + f"indexers/{self.config.indexer}"
-        req = requests.put(url, headers=self.base_headers, json=body, params=self.base_payload, **kwargs)
+        url = self.config.base_url + f"indexers/{self.config.indexer}"
+        req = requests.put(
+            url, headers=self.base_headers,
+            json=body, params=self.base_payload, **kwargs)
         if self.debug:
             print(req.text)
         req.raise_for_status()
 
-    #reset and run indexer
     def reset_run(self, reset=True, run=True, wait=False, **kwargs):
+        """Reset and run the indexer."""
         if reset:
-            reset_url = self.base_url + f"indexers/{self.config.indexer}/reset"
-            reset = requests.post(reset_url, headers=self.base_headers, params=self.base_payload, **kwargs)
+            reset_url = self.config.base_url \
+                + f"indexers/{self.config.indexer}/reset"
+            reset = requests.post(
+                reset_url, headers=self.base_headers,
+                params=self.base_payload, **kwargs)
             reset.raise_for_status()
         if run:
-            run_url = self.base_url + f"indexers/{self.config.indexer}/run"
-            run = requests.post(run_url, headers=self.base_headers, params=self.base_payload, **kwargs)
+            run_url = self.config.base_url \
+                + f"indexers/{self.config.indexer}/run"
+            run = requests.post(
+                run_url, headers=self.base_headers,
+                params=self.base_payload, **kwargs)
             run.raise_for_status()
-        status_url = self.base_url + f"indexers/{self.config.indexer}/status"
-        status = requests.get(status_url, headers=self.base_headers, params=self.base_payload, **kwargs)
+        status_url = self.config.base_url \
+            + f"indexers/{self.config.indexer}/status"
+        status = requests.get(
+            status_url, headers=self.base_headers,
+            params=self.base_payload, **kwargs)
         status.raise_for_status()
         out = status.json()
         state = out.get('lastResult').get('status')
@@ -110,18 +151,21 @@ class AzureSearch:
         if wait:
             while state == 'inProgress' or state == "reset":
                 time.sleep(10)
-                status = requests.get(status_url, headers=self.base_headers, params=self.base_payload, **kwargs)
+                status = requests.get(
+                    status_url, headers=self.base_headers,
+                    params=self.base_payload, **kwargs)
                 status.raise_for_status()
                 out = status.json()
                 state = out.get('lastResult').get('status')
                 print(state)
 
-    #query
     def query(self, query, **kwargs):
+        """Query the search service."""
         payload = self.base_payload.copy()
         payload["search"] = query
-        query_url = self.base_url + f"indexes/{self.config.index}/docs"
-        query = requests.get(query_url, headers=self.base_headers, params=payload, **kwargs)
+        query_url = self.config.base_url + f"indexes/{self.config.index}/docs"
+        query = requests.get(
+            query_url, headers=self.base_headers,
+            params=payload, **kwargs)
         query.raise_for_status()
         return query.json()
-
